b6135967e9e79ae313f1aa6f418ba1d0fafcad0f
Luong Thai Son <luongthaison2k4@gmail.com>
2023-12-01 03:23:53 +0700

    update : homework week 6

--- week-6/btvn/bai_4.py
+++ week-6/btvn/bai_4.py
@@ -1,31 +1,30 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import math
 
-width = np.array([[50,75,80,60,70,100,65,85]]).T  # traindata
-prices = np.array([[600,860,930,700,800,1150,750,970]]).T
+def is_integer(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
 
-plt.plot(width,prices , 'ro')
-plt.xlabel('width(m2)')
-plt.ylabel('prices(triệu đô)')
-# plt.show()
+def is_float(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
 
-# => đồ thị là đường thẳng nên prices = w_1 * width + w_0
-one = np.ones((width.shape[0], 1))
-widthbar = np.concatenate((one,width) , axis= 1)
-# print(pricesbar)
-A = np.dot(widthbar.T, widthbar)
-# print(A)
-b = np.dot(widthbar.T, prices)
-w = np.dot(np.linalg.pinv(A), b)
-# print("w: ",w)
+def read():
+    with open('/Users/luongthaison/Documents/năm 2023 -  SV năm 2/Python/Python-HIT-Private/week-6/btvn/input.txt', 'r') as file:
+        my_list = [i for i in file.read().split() if is_integer(i) or is_float(i)]
+    return list(map(float, my_list))
 
-w_0 = w[0][0]
-w_1 = w[1][0]
-x0 = np.linspace(45,105,2)
-y0 = w_0 + w_1*x0
+def output():
+    with open('/Users/luongthaison/Documents/năm 2023 -  SV năm 2/Python/Python-HIT-Private/week-6/btvn/output.txt', 'w') as output_file:
+        x1, y1, x2, y2 = read()
+        AB = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+        formatted_distance_AB = "{:.2f}".format(AB)
+        output_file.write(f"A({x1},{y1}) B({x2},{y2}) AB = {formatted_distance_AB}")
 
-plt.plot(width.T, prices.T , 'ro')
-plt.plot(x0,y0)
-plt.show()
-print("Dự đoán giá nhà 40m2 là:", end= " ")
-print("{:.5f}".format(w_1*40 + w_0))
+
+output()
